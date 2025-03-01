34c108a5ecddcf21ed61562761a9f4095943b1c3
pranav7Shankar <pranav7shankar@gmail.com>
2025-03-02 03:10:06 +0530

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
-import type { NextConfig } from "next";
+const nextConfig = {
+  images: {
+    dangerouslyAllowSVG:true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+    ],
+  },
+  experimental:{
+    ppr:"incremental"
+  },
+  devIndicators:{
+    appIsrStatus:true,
+    buildActivity:true,
+    buildActivityPosition:"bottom-right",
 
-const nextConfig: NextConfig = {
-  /* config options here */
+  }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
